c2f3eb599c847f1ac006bea8beb735020f843a1a
myst3r10n <myst3r10n@rocketmail.com>
2013-01-31 13:52:07 +0100

    added filter and filterNot with parallel processing support

--- widebase.db.table/src/main/scala/widebase/db/table/PartitionMap.scala
+++ widebase.db.table/src/main/scala/widebase/db/table/PartitionMap.scala
@@ -1,18 +1,78 @@
 package widebase.db.table
 
-import scala.collection.mutable.LinkedHashMap
+import java.util.concurrent.CountDownLatch
+
+import scala.actors.Futures.future
+import scala.collection.mutable. { Buffer, LinkedHashMap }
 
 /** A [[scala.collection.mutable.LinkedHashMap]] for pairs of string based partition domain and [[widebase.db.table.Table]].
  *
  * @author myst3r10n
  */
-class PartitionMap extends LinkedHashMap[String, Table] {
+class PartitionMap {
+
+  protected val map = LinkedHashMap[String, Table]()
+
+  /** Filters all tables of this partition which satisfy a predicate.
+   *
+   * @param predicate used to test elements.
+   *
+   * @return filtered table
+   */
+  def filter(predicate: Record => Boolean) = {
+
+    val partialFiltered: Buffer[Table] =
+      (for(i <- 0 to tables.size - 1)
+        yield(null)).toBuffer
+
+    var i = 0
+    val pending = new CountDownLatch(tables.size)
+
+    tables.foreach { table =>
+
+      val index = Int.box(i)
+
+      future {
+
+        try{
+
+          partialFiltered(index) = table.filter(predicate)
+
+        } finally {
+
+          pending.countDown
+
+        }
+      }
+
+      i += 1
+
+    }
+
+    pending.await
+
+    val table = partialFiltered.head
+    partialFiltered.drop(1).foreach(table ++= _)
+
+    table
+
+  }
+
+  /** Filters all tables of this partition which do not satisfy a predicate.
+   *
+   * @param predicate used to test elements.
+   *
+   * @return filtered table
+   */
+  def filterNot(predicate: Record => Boolean) = filter(!predicate(_))
 
   /** A serie of partition domains. */
-  def parts = keys
+  def parts = map.keys
+
+  def +=(pair: (String, Table)) = map += pair._1 -> pair._2
 
   /** A serie of tables. */
-  def tables = values
+  def tables = map.values
 
   /** A printable [[widebase.db.table.PartitionMap]]. */
   override def toString =
