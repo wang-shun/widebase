ab59d22fe5e8e22bf1f31014f0802160029460e6
myst3r10n <myst3r10n@rocketmail.com>
2013-02-01 02:12:58 +0100

    added note about performance and example how to use

--- widebase.db.table/src/main/scala/widebase/db/table/TemplateTable.scala
+++ widebase.db.table/src/main/scala/widebase/db/table/TemplateTable.scala
@@ -2,6 +2,47 @@ package widebase.db.table
 
 /** A common trait for ORM tables.
  *
+ * You need first a record:
+ *
+ * {{{
+ * case class Log(val time: LocalDateTime, val user: String)
+ * }}}
+ *
+ * Second create a ORM table:
+ *
+ * {{{
+ * case class LogTable(
+ *   table: Table = Table(string("time", "user"), dateTime(), string()))
+ *   extends TemplateTable[Log] {
+ *
+ *   val time = table("time").Z
+ *   val user = table("user").S
+ *
+ *   def +=(log: Log) = { time += log.time; user += log.user; this }
+ *
+ *   def +=(time: LocalDateTime, user: String): LogTable =
+ *     this += Log(time, user)
+ *
+ *   def ++=(table: LogTable) = {
+ *     for(r <- 0 to table.records.length - 1)
+ *       this += table(r)
+ *     this
+ *   }
+ *
+ *   def apply(index: Int) = Log(time(index), user(index))
+ *
+ *   def filter(predicate: Log => Boolean) = {
+ *     val filteredTable = new LogTable
+ *     for(r <- 0 to records.length - 1)
+ *       if(predicate(this(r)))
+ *         filteredTable += this(r)
+ *     filteredTable
+ *   }
+ *
+ *   def peer = table
+ * }
+ * }}}
+ *
  * @author myst3r10n
  */
 trait TemplateTable[R] {
@@ -40,6 +81,8 @@ trait TemplateTable[R] {
   /** Filters all elements of this table which satisfy a predicate.
    *
    * @param predicate used to test elements.
+   *
+   * @note Is faster than `filter` by widebase.db.table.Table
    *
    * @return filtered table
    */
