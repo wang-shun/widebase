806ab25a4be1f1f277d1622ceb94d1f492a19893
myst3r10n <myst3r10n@rocketmail.com>
2013-02-04 14:45:47 +0100

    Released 0.3.2

--- project/Build.scala
+++ project/Build.scala
@@ -207,7 +207,7 @@ object WidebaseBuild extends Build {
   /** Build settings */
 	def buildSettings = Seq(
 		organization := "com.github.widebase",
-		version := "0.3.2-SNAPSHOT",
+		version := "0.3.2",
     scalacOptions ++= Seq("-unchecked", "-deprecation"),
     resolvers ++= Seq(
       "Sonatype OSS" at "https://oss.sonatype.org/content/groups/public"))
