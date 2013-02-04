efa8ac5fe875ecc9831ceece2225594405b012b3
myst3r10n <myst3r10n@rocketmail.com>
2013-02-04 13:20:15 +0100

    fixed UpnarsableMessage hanling

--- widebase.stream.handler.rq/src/main/scala/widebase/stream/handler/rq/ConsumerHandler.scala
+++ widebase.stream.handler.rq/src/main/scala/widebase/stream/handler/rq/ConsumerHandler.scala
@@ -104,8 +104,8 @@ class ConsumerHandler(listener: RecordListener)
       case message: EventMessage => reader! message
       case message: RollbackMessage => reader ! message
       case message: TableMessage => reader ! message
-      case message: UnparsableMessage => Channels.fireMessageReceived(ctx, message)
-      case message: UnauthorizedMessage => Channels.fireMessageReceived(ctx, message)
+      case message: UnparsableMessage => reader ! message
+      case message: UnauthorizedMessage =>  Channels.fireMessageReceived(ctx, message)
       case message => error("Unfamiliar with message: " + message)
 
     }
@@ -157,6 +157,17 @@ class ConsumerHandler(listener: RecordListener)
           try {
 
             listener.react(Table.fromBytes(message.bytes))
+
+          } catch {
+
+            case e => e.printStackTrace
+
+          }
+
+        case message: UnparsableMessage =>
+          try {
+
+            listener.react(message)
 
           } catch {
 
