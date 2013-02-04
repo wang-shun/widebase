7d62a685ec5625173ff50b5514490edb32a46f59
myst3r10n <myst3r10n@rocketmail.com>
2013-02-04 12:02:46 +0100

    Wraps selector code

---
+++ widebase.stream.handler.rq/src/main/scala/widebase/stream/handler/rq/Selector.scala
@@ -0,0 +1,14 @@
+package widebase.stream.handler.rq
+
+import widebase.db.table.Table
+
+/** Table selector.
+ *
+ * @author myst3r10n
+ */
+class Selector {
+
+  def apply(table: Table): Table = null
+
+}
+
