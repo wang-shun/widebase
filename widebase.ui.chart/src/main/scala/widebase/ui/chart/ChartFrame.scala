7214dc2286c0cd5a0b01a7a7e856e2ddb2926685
myst3r10n <myst3r10n@rocketmail.com>
2013-02-14 21:41:15 +0100

    Remoted obsolete code

--- widebase.ui.chart/src/main/scala/widebase/ui/chart/ChartFrame.scala
+++ widebase.ui.chart/src/main/scala/widebase/ui/chart/ChartFrame.scala
@@ -77,53 +77,5 @@ class ChartFrame(protected var panel0: ChartPanel = null) extends LFrame {
     case ChartZoomOutY => panel.peer.zoomOutRange(0.0, 0.0)
 
   }
-  /** Replace chart panel.
-   *
-   * @param panel to replace
-   *
-   * @return frame
-   *//*
-  def set(panel: ChartPanel with Publisher) = {
-
-    this.panel0 = panel
-
-    val toolBar = new ChartToolBar
-
-    val scrollPane = new ScrollPane {
-
-      contents = new Component {
-
-        ChartFrame.this.deafTo(ChartFrame.this.panel)
-        ChartFrame.this.listenTo(panel)
-
-        override lazy val peer = panel
-
-      }
-    }
-
-    contents = new scala.swing.BorderPanel {
-
-      peer.add(toolBar, BorderLayout.NORTH)
-      add(scrollPane, BorderPanel.Position.Center)
-
-    }
-
-    listenTo(this, toolBar)
-
-    reactions += {
-
-      case ChartZoomIn => panel.zoomInBoth(0.0, 0.0)
-      case ChartZoomInX => panel.zoomInDomain(0.0, 0.0)
-      case ChartZoomInY => panel.zoomInRange(0.0, 0.0)
-      case event: ChartZoomMouse => panel.setMouseZoomable(event.enabled)
-      case ChartZoomOut => panel.zoomOutBoth(0.0, 0.0)
-      case ChartZoomOutX => panel.zoomOutDomain(0.0, 0.0)
-      case ChartZoomOutY => panel.zoomOutRange(0.0, 0.0)
-
-    }
-
-    this
-
-  }*/
 }
 
