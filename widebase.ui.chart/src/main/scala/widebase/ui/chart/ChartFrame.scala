22e25ce0ada53e9bae37bb761fcf936a2749db93
myst3r10n <myst3r10n@rocketmail.com>
2013-02-22 15:47:46 +0100

    Added/Removed events

--- widebase.ui.chart/src/main/scala/widebase/ui/chart/ChartFrame.scala
+++ widebase.ui.chart/src/main/scala/widebase/ui/chart/ChartFrame.scala
@@ -2,10 +2,12 @@ package widebase.ui.chart
 
 import event. {
 
+  ChartMouseDragShiftable,
+  ChartMouseWheelZoomable,
+  ChartMouseZoomable,
   ChartZoomIn,
   ChartZoomInX,
   ChartZoomInY,
-  ChartZoomMouse,
   ChartZoomOut,
   ChartZoomOutX,
   ChartZoomOutY
@@ -71,10 +73,12 @@ class ChartFrame(protected var panel0: ChartPanel = null) extends LFrame {
     case ChartZoomIn => panel.peer.zoomInBoth(0.0, 0.0)
     case ChartZoomInX => panel.peer.zoomInDomain(0.0, 0.0)
     case ChartZoomInY => panel.peer.zoomInRange(0.0, 0.0)
-    case event: ChartZoomMouse => panel.peer.setMouseZoomable(event.enabled)
     case ChartZoomOut => panel.peer.zoomOutBoth(0.0, 0.0)
     case ChartZoomOutX => panel.peer.zoomOutDomain(0.0, 0.0)
     case ChartZoomOutY => panel.peer.zoomOutRange(0.0, 0.0)
+    case event: ChartMouseDragShiftable => panel.peer.mouseDragShiftable = event.enabled
+    case event: ChartMouseWheelZoomable => panel.peer.mouseWheelZoomable = event.enabled
+    case event: ChartMouseZoomable => panel.peer.setMouseZoomable(event.enabled)
 
   }
 }
