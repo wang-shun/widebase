484949a0af73902edc89b76bfc70fb178fd9ba46
myst3r10n <myst3r10n@rocketmail.com>
2013-03-17 00:47:27 +0100

    changed name of plugin folder

--- etc/ide.scala
+++ etc/ide.scala
@@ -7,8 +7,8 @@ new ConfigLike {
   val app = "val app = new widebase.ui.ide.App"
 
   val plugins = Array[File](
-    new File("plugins/widebase.ui.ide.cli/Launch.scala"),
-    new File("plugins/widebase.ui.ide.editor/Launch.scala"))
+    new File("plugin/widebase.ui.ide.cli/Launch.scala"),
+    new File("plugin/widebase.ui.ide.editor/Launch.scala"))
 
 }
 
