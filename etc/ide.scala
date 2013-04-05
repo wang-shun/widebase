67324c0613e5df249b1c33a8b056fa00209d3fad
myst3r10n <myst3r10n@rocketmail.com>
2013-04-05 11:51:37 +0200

    Modified plugin registration

--- etc/ide.scala
+++ etc/ide.scala
@@ -1,16 +1,15 @@
-import java.io.File
-
 import widebase.workspace.runtime.ConfigLike
 
 new ConfigLike {
 
   val app = "val app = new widebase.workspace.ide.App"
 
-  val plugins = Array[File](
-    new File("plugin/widebase.workspace.ide.cli/Launch.scala"),
-    new File("plugin/widebase.workspace.ide.table/Launch.scala"),
-    new File("plugin/widebase.workspace.ide.chart/Launch.scala"),
-    new File("plugin/widebase.workspace.ide.editor/Launch.scala"))
+  val plugins = Array[String](
+    "widebase.workspace.ide.cli",
+    "widebase.workspace.ide.explorer",
+    "widebase.workspace.ide.table",
+    "widebase.workspace.ide.chart",
+    "widebase.workspace.ide.editor")
 
 }
 
