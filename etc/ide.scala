78f2b8ef2356f5b225601a656a9d3d5f08a17e65
myst3r10n <myst3r10n@rocketmail.com>
2013-04-12 01:46:15 +0200

    rewrite

--- etc/ide.scala
+++ etc/ide.scala
@@ -2,9 +2,8 @@ import widebase.workspace.runtime.ConfigLike
 
 new ConfigLike {
 
-  val app = "val app = new widebase.workspace.ide.App"
-
-  val plugins = Array[String](
+  val plugin = Array[String](
+    "widebase.workspace.ide.app",
     "widebase.workspace.ide.cli",
     "widebase.workspace.ide.explorer",
     "widebase.workspace.ide.table",
